685826a7ed29c02e271c93cfd6c545a81d88f15c
RamVegiraju <rav3sa@virginia.edu>
2020-08-28 00:49:07 -0400

    Added functions in script

--- functions.py
+++ functions.py
@@ -1,14 +1,38 @@
-#functions for model inference
+#functions for model building and EDA
+from sklearn.preprocessing import OrdinalEncoder
 
-#Checking for favorite artists in list
-def intersection(lst1, lst2): 
-    lst3 = [value for value in lst1 if value in lst2] 
-    if not lst3:
-        print("There are no common artists")
-        return False
-    else:
-        return True
+#Encoding the categorical variables
+def encodeData(df):
+    enc = OrdinalEncoder()
+    encodedVariables = df[['Genre/Mood','Language']]
+    enc.fit(encodedVariables[["Genre/Mood","Language"]])
+    encodedVariables[["Genre/Mood","Language"]] = enc.transform(encodedVariables[["Genre/Mood","Language"]])
+    df['encodedLanguage'] = encodedVariables['Language']
+    df['encodedGenre'] = encodedVariables['Genre/Mood']
+    return df
 
-lst1 = ['Ram', 'Nadal', 'Djokovic']
-lst2 = ['Ram']
-print(intersection(lst1,lst2))
+#Model Creation
+def model(df):
+    data = df[['encodedGenre','encodedLanguage']]
+    model_knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=150, n_jobs=-1).fit(data)
+    distances, indices = model_knn.kneighbors(data)
+    return model_knn
+
+def getPredictions(genre, language, favorite_artists, playlistSize):
+    """
+    Factors in genre, language, list of favorite artists, and the size of the playlist you want
+    """
+    results = model_knn.kneighbors([[genre, language]])
+    results = results[1]
+    resultList = results.tolist()
+    resultList = resultList[0]
+    playlist = df.loc[resultList]
+    playlist = playlist[['name','artist','popularity','release_year']]
+    playlist['ContainsArtist'] = playlist['artist'].apply(lambda x: any([k in x for k in favorite_artists])) #checks for favorite artist
+    playlist = playlist.sort_values(["ContainsArtist","popularity"],ascending = (False, False))
+    notPopular = playlist[playlist['popularity'] < 40].index
+    playlist.drop(notPopular, inplace=True)
+    return playlist.head(playlistSize)
+
+
+#print("All is good right now")
