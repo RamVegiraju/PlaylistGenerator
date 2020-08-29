52362249ed598c2f9e33207431052ccbb58f6744
RamVegiraju <rav3sa@virginia.edu>
2020-08-28 22:31:03 -0400

    Playlist generator form working through flask

--- app.py
+++ app.py
@@ -4,10 +4,20 @@ import requests
 import pandas as pd
 import numpy as np
 
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+if __name__ =='__main__':
+    app.run(debug=True)
+
+
+"""
 @app.route('/', methods = ["GET","POST"]) 
 def index():
     if request.method == "POST":
         input_text = request.form.get('url')
         print("This is a test right now.")
     return render_template("index.html")
+"""
